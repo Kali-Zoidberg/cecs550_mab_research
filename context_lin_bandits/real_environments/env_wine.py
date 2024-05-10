e6e6668c3fdd7475004c3cbdf8a0f021151496a3
alexyung <48598834+alexyung@users.noreply.github.com>
2024-05-10 15:45:51 -0700

    added dataset loading to env_wine in case we want to skip the preprocess steps altogether

--- context_lin_bandits/real_environments/env_wine.py
+++ context_lin_bandits/real_environments/env_wine.py
@@ -8,34 +8,39 @@ from numba import jit
 import torch
 import torch.nn as nn
 
+from ucimlrepo import fetch_ucirepo
+
 from .utils import *
 
 
 def Load_Wine(model_tail, num_partial, device):
     # assuming the program is run through the same means as the other datasets, data will be written to directory in 
     # wine_preprocess.py the same way as the other datasets.
-    '''
+    
+    #assuming we don't drop any data here since there isn't anything irrelevant like user/movie ID
+    dataset = fetch_ucirepo(id=186)
+
+    data = pd.concat((dataset.data.features, dataset.data.targets), axis=1)
+
+    #for each sample, we set reward to 0 if quality < 5, otherwise 1
+    data["reward"] = np.where(data["quality"] < 5, 0, 1)
+    data.pop("quality")
+    data = data.reset_index(drop=True)
+
     data_array = data.to_numpy()
-    # fetch dataset
-    wine_quality = fetch_ucirepo(id=186)
-    # data (as pandas dataframes)
-    # define cntext (features)
-    X = wine_quality.data.features
-    Y = wine_quality.data.targets
 
-    X_split = []
-    for i in range(data_array.shape[0]):
-        user_id = data_array[i, 0]
-        movie_id = data_array[i, 1]
+    #get the rewards as a separate vector
+    #dataframe is 11 attributes then the reward, so get idx 11
+    Y = data_array[:, 11]
 
-        X_split.append(np.concatenate((user_id_to_feature[user_id], movie_id_to_feature[movie_id])).copy())
-    X = np.vstack(X_split)
+    X = data_array[:, :11]
+
     reward0_idx = np.where(Y == 0)[0]
     reward1_idx = np.where(Y == 1)[0]
-    '''
+
     #X0 contains samples with reward 0; X1 with reward 1
-    X0 = np.load('./real_datasets/wine/preprocess/X0_wine.npy')
-    X1 = np.load('./real_datasets/wine/preprocess/X1_wine.npy')
+    X0 = X[reward0_idx, :]
+    X1 = X[reward1_idx, :]
 
     if num_partial > 0:
         X0_len = X0.shape[0]
@@ -56,7 +61,7 @@ def Load_Wine(model_tail, num_partial, device):
     autoencoder.eval()
     return autoencoder, X0, X1, emb_dim
 
-
+# the class base_Env is found in real_environments/utils.py
 class wine_Env(base_Env):
     def __init__(self, args):
         super().__init__(args, Load_Wine)
